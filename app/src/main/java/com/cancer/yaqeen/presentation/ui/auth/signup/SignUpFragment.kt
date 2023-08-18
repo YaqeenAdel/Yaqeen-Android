e365f41e2d38c1fcf2e44796e8d7ac084b79fbe3
mark.khali <mkhalil@trycar.com>
2023-08-18 20:18:43 +0300

    --Intialized Auth0 in the project --Finished Login With Auth0

--- app/src/main/java/com/cancer/yaqeen/presentation/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/cancer/yaqeen/presentation/ui/auth/signup/SignUpFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cancer.yaqeen.R
+import com.cancer.yaqeen.databinding.FragmentLoginBinding
+import com.cancer.yaqeen.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
-
+    private var _binding: FragmentSignUpBinding? = null
+    private var binding: FragmentSignUpBinding? = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return _binding?.root
     }
 
 }
---
+++ app/src/main/java/com/cancer/yaqeen/presentation/ui/auth/signup/SignupViewModel.kt
@@ -0,0 +1,4 @@
+package com.cancer.yaqeen.presentation.ui.auth.signup
+
+class SignupViewModel {
+}
