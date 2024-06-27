fda2817958342c305da02e2fce008b74979ea2c5
Mohamed.AshrafAli <mohamed.ashrafali@ibnsina-pharma.com>
2024-06-27 21:23:25 +0300

    Rollback to last libraries versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,8 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
     kotlin("kapt")
-//    id("dagger.hilt.android.plugin")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
@@ -22,7 +21,6 @@ val keystoreProperties = Properties()
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
-    namespace = "com.cancer.yaqeen"
     compileSdk = 34
 
     defaultConfig {
@@ -31,7 +29,6 @@ android {
         targetSdk = 34
         versionCode = 3
         versionName = "1.0.3"
-        multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["auth0Domain"] =  "yaqeen.eu.auth0.com"
         manifestPlaceholders["auth0Scheme"] =  "demo"
@@ -51,16 +48,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         viewBinding = true
         dataBinding = true
-        buildConfig = true
     }
 }
 
@@ -75,10 +71,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-//    kapt("com.google.dagger:hilt-android-compiler:2.44")
-//    kapt("androidx.hilt:hilt-compiler:1.2.0")
-    kapt("com.google.dagger:hilt-compiler:2.49")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
     implementation("androidx.hilt:hilt-work:1.2.0")
 
     // Firebase
@@ -169,7 +164,4 @@ dependencies {
     // chuck interceptor
     debugImplementation("com.github.chuckerteam.chucker:library:3.5.2")
     releaseImplementation("com.github.chuckerteam.chucker:library-no-op:3.5.2")
-}
-kapt {
-    correctErrorTypes = true
 }
